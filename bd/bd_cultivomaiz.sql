b63a0d2ccdefb35cd12d06edf542ec874ac56756
kerenArias-2001 <kerenesther2001@gmail.com>
2023-08-03 20:12:40 -0500

    export "bd" 03/agosto

--- bd/bd_cultivomaiz.sql
+++ bd/bd_cultivomaiz.sql
@@ -2,7 +2,7 @@
 SQLyog Ultimate v11.11 (64 bit)
 MySQL - 5.5.5-10.4.24-MariaDB : Database - bd_cultivomaiz
 *********************************************************************
-*/
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -28,36 +28,14 @@ CREATE TABLE `inventario` (
   `precio_unidad` varchar(255) DEFAULT NULL,
   `tiempo_alq` varchar(255) DEFAULT NULL,
   `subtotal_alquiler` varchar(255) DEFAULT NULL,
-  `datos_usuarios` int(100) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_usuario` (`datos_usuarios`),
-  CONSTRAINT `fk_usuario` FOREIGN KEY (`datos_usuarios`) REFERENCES `usuario` (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
 
 /*Data for the table `inventario` */
 
 LOCK TABLES `inventario` WRITE;
 
-insert  into `inventario`(`id`,`nombre_producto`,`tipo_inventario`,`cantidad`,`precio_unidad`,`tiempo_alq`,`subtotal_alquiler`,`datos_usuarios`) values (1,'','alquilar','','','',NULL,36);
-
-UNLOCK TABLES;
-
-/*Table structure for table `usuario` */
-
-DROP TABLE IF EXISTS `usuario`;
-
-CREATE TABLE `usuario` (
-  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre_u` varchar(255) DEFAULT NULL,
-  `pss_u` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
-
-/*Data for the table `usuario` */
-
-LOCK TABLES `usuario` WRITE;
-
-insert  into `usuario`(`id_usuario`,`nombre_u`,`pss_u`) values (1,'user1',NULL),(2,'user2',NULL),(3,'user3',NULL),(4,'user4',NULL),(5,'user5',NULL),(6,'user6',NULL),(7,'user7',NULL),(8,'user8',NULL),(9,'user9',NULL),(10,'user10',NULL),(11,'user11',NULL),(12,'user12',NULL),(13,'user13',NULL),(14,'user14',NULL),(15,'user15',NULL),(16,'user16',NULL),(17,'user17',NULL),(18,'user18',NULL),(19,'user19',NULL),(20,'user20',NULL),(21,'user21',NULL),(22,'user22',NULL),(23,'user23',NULL),(24,'user24',NULL),(25,'user25',NULL),(26,'user26',NULL),(27,'user27',NULL),(28,'user28',NULL),(29,'user29',NULL),(30,'user30',NULL),(31,'user31',NULL),(32,'user32',NULL),(33,'user33',NULL),(34,'user34',NULL),(35,'user35',NULL),(36,'user36',NULL),(37,'user37',NULL),(38,'user38',NULL),(39,'user39',NULL),(40,'user40',NULL),(41,'user41',NULL),(42,'user42',NULL);
+insert  into `inventario`(`id`,`nombre_producto`,`tipo_inventario`,`cantidad`,`precio_unidad`,`tiempo_alq`,`subtotal_alquiler`) values (59,'ujgb','comprar','','',NULL,NULL);
 
 UNLOCK TABLES;
 
@@ -73,14 +51,33 @@ CREATE TABLE `terreno` (
   `id_user` int(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_usuario` (`id_user`),
-  CONSTRAINT `fk_usuario` FOREIGN KEY (`id_user`) REFERENCES `bd_cultivomaiz`.`usuario` (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  CONSTRAINT `fk_usuario` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 /*Data for the table `terreno` */
 
 LOCK TABLES `terreno` WRITE;
 
-insert  into `terreno`(`id`,`area`,`surcos`,`distancia`,`id_user`) values (1,'1','2','3',1);
+insert  into `terreno`(`id`,`area`,`surcos`,`distancia`,`id_user`) values (1,'10000','75','25',1),(2,'129',NULL,NULL,NULL),(3,'5',NULL,NULL,NULL),(4,'4',NULL,NULL,NULL),(5,'4',NULL,NULL,NULL),(6,'4',NULL,NULL,NULL),(7,'',NULL,NULL,NULL),(8,'',NULL,NULL,NULL),(9,'1',NULL,NULL,NULL),(10,'7',NULL,NULL,NULL),(11,'4',NULL,NULL,NULL),(12,'5',NULL,NULL,NULL);
+
+UNLOCK TABLES;
+
+/*Table structure for table `usuario` */
+
+DROP TABLE IF EXISTS `usuario`;
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_u` varchar(255) DEFAULT NULL,
+  `pss_u` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+
+/*Data for the table `usuario` */
+
+LOCK TABLES `usuario` WRITE;
+
+insert  into `usuario`(`id_usuario`,`nombre_u`,`pss_u`) values (1,'user1',NULL),(2,'user2',NULL),(3,'user3',NULL),(4,'user4',NULL),(5,'user5',NULL),(6,'user6',NULL),(7,'user7',NULL),(8,'user8',NULL),(9,'user9',NULL),(10,'user10',NULL),(11,'user11',NULL),(12,'user12',NULL),(13,'user13',NULL),(14,'user14',NULL),(15,'user15',NULL),(16,'user16',NULL),(17,'user17',NULL),(18,'user18',NULL),(19,'user19',NULL),(20,'user20',NULL),(21,'user21',NULL);
 
 UNLOCK TABLES;
 
