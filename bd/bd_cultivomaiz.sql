9b8fded2ab7fcf8e91dc291de45eddceeeb2cdee
kerenArias-2001 <kerenesther2001@gmail.com>
2023-08-04 07:28:42 -0500

    4/08/2023

--- bd/bd_cultivomaiz.sql
+++ bd/bd_cultivomaiz.sql
@@ -2,8 +2,7 @@
 SQLyog Ultimate v11.11 (64 bit)
 MySQL - 5.5.5-10.4.24-MariaDB : Database - bd_cultivomaiz
 *********************************************************************
-*/
-
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -29,19 +28,17 @@ CREATE TABLE `inventario` (
   `precio_unidad` varchar(255) DEFAULT NULL,
   `tiempo_alq` varchar(255) DEFAULT NULL,
   `subtotal_alquiler` varchar(255) DEFAULT NULL,
-  
-  PRIMARY KEY (`id`),
   `id_user1` int(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
   KEY `fk_usuario` (`id_user1`),
   CONSTRAINT `fk_usuario1` FOREIGN KEY (`id_user1`) REFERENCES `usuario` (`id_usuario`)
-
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `inventario` */
 
 LOCK TABLES `inventario` WRITE;
 
-insert  into `inventario`(`id`,`nombre_producto`,`tipo_inventario`,`cantidad`,`precio_unidad`,`tiempo_alq`,`subtotal_alquiler`) values (59,'ujgb','comprar','','',NULL,NULL);
+insert  into `inventario`(`id`,`nombre_producto`,`tipo_inventario`,`cantidad`,`precio_unidad`,`tiempo_alq`,`subtotal_alquiler`,`id_user1`) values (1,'Tractor','alquilar','1','100000','2',NULL,2),(2,'Mangueras','comprar','12','20000',NULL,NULL,2);
 
 UNLOCK TABLES;
 
@@ -54,22 +51,21 @@ CREATE TABLE `terreno` (
   `area` varchar(255) DEFAULT NULL,
   `surcos` varchar(255) DEFAULT NULL,
   `distancia` varchar(255) DEFAULT NULL,
- 
+  `id_user` int(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-   `id_user` int(255) DEFAULT NULL,
   KEY `fk_usuario` (`id_user`),
   CONSTRAINT `fk_usuario` FOREIGN KEY (`id_user`) REFERENCES `usuario` (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `terreno` */
 
 LOCK TABLES `terreno` WRITE;
 
-insert  into `terreno`(`id`,`area`,`surcos`,`distancia`,`id_user`) values (1,'10000','75','25',1),(2,'129',NULL,NULL,NULL),(3,'5',NULL,NULL,NULL),(4,'4',NULL,NULL,NULL),(5,'4',NULL,NULL,NULL),(6,'4',NULL,NULL,NULL),(7,'',NULL,NULL,NULL),(8,'',NULL,NULL,NULL),(9,'1',NULL,NULL,NULL),(10,'7',NULL,NULL,NULL),(11,'4',NULL,NULL,NULL),(12,'5',NULL,NULL,NULL);
+insert  into `terreno`(`id`,`area`,`surcos`,`distancia`,`id_user`) values (1,'10000','90','25',1),(2,'1200000','90','25',2);
 
 UNLOCK TABLES;
 
-/*Table structure for table  `usuario`- */
+/*Table structure for table `usuario` */
 
 DROP TABLE IF EXISTS `usuario`;
 
@@ -78,13 +74,13 @@ CREATE TABLE `usuario` (
   `nombre_u` varchar(255) DEFAULT NULL,
   `pss_u` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `usuario` */
 
 LOCK TABLES `usuario` WRITE;
 
-insert  into `usuario`(`id_usuario`,`nombre_u`,`pss_u`) values (1,'user1',NULL),(2,'user2',NULL),(3,'user3',NULL),(4,'user4',NULL),(5,'user5',NULL),(6,'user6',NULL),(7,'user7',NULL),(8,'user8',NULL),(9,'user9',NULL),(10,'user10',NULL),(11,'user11',NULL),(12,'user12',NULL),(13,'user13',NULL),(14,'user14',NULL),(15,'user15',NULL),(16,'user16',NULL),(17,'user17',NULL),(18,'user18',NULL),(19,'user19',NULL),(20,'user20',NULL),(21,'user21',NULL);
+insert  into `usuario`(`id_usuario`,`nombre_u`,`pss_u`) values (1,'user1',NULL),(2,'user2',NULL);
 
 UNLOCK TABLES;
 
