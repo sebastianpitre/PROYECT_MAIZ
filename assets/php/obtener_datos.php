74a52eeabfbe761febcb45fa19845494235494c4
kerenArias-2001 <kerenesther2001@gmail.com>
2023-08-04 11:40:52 -0500

    cant_toneladas 04/agosto

--- assets/php/obtener_datos.php
+++ assets/php/obtener_datos.php
@@ -18,7 +18,7 @@ include('funciones.php');
   $dist_semillasm = intval($dist_semillas)/100;
   $dist_surcosm = intval($dist_surcos)/100;
   $cant_plantas = round(intval($area)/($dist_semillasm*$dist_surcosm));
-  $cant_obtenida = (($cant_planta*300)/1000) ;
+  $cant_obtenida = (($cant_plantas*300)/1000) ;
   $area=$area = intval($area*10000);
   $area=$area ." m²";
   $dist_semillas = $dist_semillas ." cm";
